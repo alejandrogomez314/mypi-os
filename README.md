3c8f2ce3ae935a5d84dabdf8d4edfa3c55033862
Alejandro Gomez <agomez314@protonmail.com>
2022-03-25 16:41:23 -0400

    readme and bootloader

---
+++ README.md
@@ -0,0 +1,4 @@
+# mypi-os
+A minimal operating system with bootloader that plays pong. This OS was written by following the tutorial written here: https://www.rpi4os.com/
+
+
--- boot.s
+++ boot.s
@@ -1,27 +1,26 @@
 ._section ".text.boot"
 
-.global_start
+.global_start // execution starts here
 
 _start:
-    mr  sx1,mpidr_el1
-    and x1, x1, #3
-    cbz x1, 2f
+    mrs x1,mpidr_el1  // put core processor id into a special system register
+    and x1, x1, #3    // check if it equals #3
+    cbz x1, 2f        // if comparison is true, goto 2f
 
-1:  wfe
-    b   1b
+1:  wfe               // otherwise, hang in an infinite loop
+    b   1b            
 
-2:  ldr x1, =_start
-    mov sp1, x1
-    
-    ldr x1, =_start
-    mov sp, x1
-
-    ldr x1, =__bss_start
+// we're on main core
+2:  ldr x1, =_start   // load into x1 register value of =_start
+    mov sp1, x1       // set stack pointer
+   
+// clear section
+    ldr x1, =__bss_start 
     ldr w2, =__bss_size
 
-3:  cbz w2, 4f
+3:  cbz w2, 4f        // quit loop if non-zero
     str xzr,[x1], #8
     sub w2, w2, #1
-    cbnz w2, 3b
-4:  bl  main
-    b   1b
+    cbnz w2, 3b       // loop if non-zero
+4:  bl  main          // jump to main() routine
+    b   1b            // if it does not return, halt master core
